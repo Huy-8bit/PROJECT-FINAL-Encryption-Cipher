76eb07b0e78f5c4a7d64cf55bb4db713ba83eb00
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-12-31 01:19:14 +0700

    new

--- SHA/document.py
+++ SHA/document.py
@@ -128,3 +128,112 @@ if __name__ == "__main__":
     hash = Sha512(u'The quick brown fox jumps over the lazy dog')
     hash = ''.join('{:02x}'.format(i) for i in hash)
     print(wrap(hash, 8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    h0 = 0xc1059ed8
+    h1 = 0x367cd507
+    h2 = 0x3070dd17
+    h3 = 0xf70e5939
+    h4 = 0xffc00b31
+    h5 = 0x68581511
+    h6 = 0x64f98fa7
+    h7 = 0xbefa4fa4
+    k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 
+         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 
+         0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 
+         0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
+         0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 
+         0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 
+         0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
+         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+    
+    
+    
+    
+import hashlib
+
+p = 0;
+q = 0;
+text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!,."
+publicKey = 0
+privateKey = 0
+plainText = ""
+encryptedText = ""
+word = ""
+keyWord = ""
+keyNumber = 1
+
+data = input("Alice enter message: ")
+# Generate the SHA-1 hash
+sha1_hash = hashlib.sha1(data.encode())
+
+# Get the hexadecimal representation of the hash
+hex_hash = sha1_hash.hexdigest()
+
+print("After hash using SHA: " ,hex_hash);
+
+#RSA
+p, q = input("enter two key PRIME numbers seperated by space ").split(" ")
+p = int(p)
+q = int(q)
+n = p * q
+phiN = (p-1) * (q-1)
+
+for i in range(1, 10000):
+    if 0 != phiN % i:
+        publicKey = round(i)
+        break
+
+for j in range(0, 10000):
+    d = 1 + (j * phiN)
+    d2 = d / publicKey
+    if 0 == d % publicKey:
+        privateKey = round(d2)
+        break
+    
+print("p=",p)
+print("q=",q)
+print("modulo=",n)
+print("public key=",publicKey)
+print("private key=",privateKey)
+
+encryptedText = ""
+for k in data:
+    m = 0
+    for l in text:
+        if k == l:
+            if m < 10:
+                m = m + 00
+            encryptedText = encryptedText + (str((m ** publicKey) % n)) + " "
+            break
+        m+=1
+print("Encrypted message : ", encryptedText)
+
+decryptedText = ""
+#decryptedText = input("Enter encrypted text: ")
+# plainText = ""
+for s in encryptedText.split(" "):
+    for k in text:
+        m = 0
+        for l in text:
+            if k == l:
+                if s == (str((m ** publicKey) % n)):
+                    decryptedText = decryptedText + l
+                break
+            m += 1
+
+print("Bob receive message: ", decryptedText)
