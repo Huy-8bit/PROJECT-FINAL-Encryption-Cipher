8ca0b786650e1ecc886ce6aa396932576ce470b4
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-12-30 20:41:09 +0700

    new

--- rsa.py
+++ rsa.py
@@ -1,61 +1,83 @@
-# p = 0;
-# q = 0;
-# text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!,."
-# publicKey = 0
-# privateKey = 0
-# plainText = ""
-# encryptedText = ""
-# word = ""
-# keyWord = ""
-# keyNumber = 1
+import random
+import os
 
-# p, q = input("enter two key PRIME numbers seperated by space ").split(" ")
-# p = int(p)
-# q = int(q)
-# n = p * q
-# phiN = (p-1) * (q-1)
+def random_prime(size_bit):
+    while True:
+        # Generate a random number with the specified number of bits
+        p = random.getrandbits(size_bit)
+        # Set the 2 least significant bits to 1 (to ensure p is odd)
+        p |= 3
+        if is_prime(p):
+            
+            return p
 
-# for i in range(1, 10000):
-#     if 0 != phiN % i:
-#         publicKey = round(i)
-#         break
+def is_prime(n):
+    if n in (2, 3):
+        return True
+    if n == 1 or n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-# for j in range(0, 10000):
-#     d = 1 + (j * phiN)
-#     d2 = d / publicKey
-#     if 0 == d % publicKey:
-#         privateKey = round(d2)
-#         break
+
+
+text = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!,. "
+
+
+
+
+
+p = random_prime(8)
+print("p=",p)
+q = random_prime(8)
+print("q=",q)
+p = int(p)
+q = int(q)
+n = p * q
+phiN = (p-1) * (q-1)
+
+for i in range(1, 10000):
+    if 0 != phiN % i:
+        publicKey = round(i)
+        break
+
+for j in range(0, 10000):
+    d = 1 + (j * phiN)
+    d2 = d / publicKey
+    if 0 == d % publicKey:
+        privateKey = round(d2)
+        break
     
-# print("p=",p)
-# print("q=",q)
-# print("modulo=",n)
-# print("public key=",publicKey)
-# print("private key=",privateKey)
-# plainText = input("Enter plain text: ")
-# encryptedText = ""
-# for k in plainText:
-#     m = 0
-#     for l in text:
-#         if k == l:
-#             if m < 10:
-#                 m = m + 00
-#             encryptedText = encryptedText + (str((m ** publicKey) % n)) + " "
-#             break
-#         m+=1
-# print("Encrypted text: ", encryptedText)
 
-# decryptedText = ""
-# for s in encryptedText.split(" "):
-#     for k in text:
-#         m = 0
-#         for l in text:
-#             if k == l:
-#                 if s == (str((m ** publicKey) % n)):
-#                     decryptedText = decryptedText + l
-#                 break
-#             m += 1
+print("modulo=",n)
+print("public key=",publicKey)
+print("private key=",privateKey)
+plainText = input("Enter plain text: ")
+encryptedText = ""
+for k in plainText:
+    m = 0
+    for l in text:
+        if k == l:
+            if m < 10:
+                m = m + 00
+            encryptedText = encryptedText + (str((m ** publicKey) % n)) + " "
+            break
+        m+=1
+print("Encrypted text: ", encryptedText)
 
-# print("Decrypted text: ", decryptedText)
+decryptedText = ""
+for s in encryptedText.split(" "):
+    for k in text:
+        m = 0
+        for l in text:
+            if k == l:
+                if s == (str((m ** publicKey) % n)):
+                    decryptedText = decryptedText + l
+                break
+            m += 1
+
+print("Decrypted text: ", decryptedText)
 
 
--- test.py
+++ test.py
@@ -1,18 +1,7 @@
-# import base64
-# from Crypto.Cipher import AES
+import random
 
-# # Set the key and initialization vector (IV)
-# key = b"0123456789abcdef0123456789abcdef"  # 32-byte key
-# iv = b"0123456789abcdef"  # 16-byte IV
+# Generate a random 32-bit integer
+x = random.getrandbits(512)
+print(x)
 
-# # Initialize the cipher
-# cipher = AES.new(key, AES.MODE_CFB, iv)
 
-# # Encrypt some data
-# data = b"Hello, World!"
-# ciphertext = cipher.encrypt(data)
-
-# # Encode the ciphertext as base64
-# encoded_ciphertext = base64.b64encode(ciphertext)
-
-# print(f"Ciphertext: {encoded_ciphertext}")
